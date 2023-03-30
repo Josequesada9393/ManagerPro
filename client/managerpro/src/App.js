d340e6d281e274fafe6e898f93cf5ad9ed8f21d4
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-03-29 19:23:14 -0700

    added layout, navigate and theme

--- client/managerpro/src/App.js
+++ client/managerpro/src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from "@mui/material/styles"
+import { themeSettings } from './theme';
+import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from './scenes/Dashboard/Dashboard.jsx'
+import Layout from "scenes/Layout/Layout.jsx"
+
+
 
 function App() {
+
+  const mode = useSelector((state) => state.global.mode);
+  //only change the theme when the mode state in the global state changes. Otherwise, memoize
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+
   return (
     <div className="App">
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
 
+            <Route element={<Layout/>}>
+              <Route path='/' element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard/>}/>
+            </Route>
+          </Routes>
+       </ThemeProvider>
+    </BrowserRouter>
     </div>
   );
 }
