6c48c739dc0af5bb9431a9c24218d3d19db9bff8
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-03-30 20:42:43 -0700

    fixed react hooks bug and added navbar

--- client/managerpro/src/App.js
+++ client/managerpro/src/App.js
@@ -3,13 +3,15 @@ import { createTheme } from "@mui/material/styles"
 import { themeSettings } from './theme';
 import { useSelector } from 'react-redux';
 import { useMemo } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import Dashboard from './scenes/Dashboard/Dashboard.jsx'
 import Layout from "scenes/Layout/Layout.jsx"
-
+import React from 'react';
 
 
 function App() {
+
+
 
   const mode = useSelector((state) => state.global.mode);
   //only change the theme when the mode state in the global state changes. Otherwise, memoize
@@ -21,10 +23,9 @@ function App() {
       <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-
             <Route element={<Layout/>}>
               <Route path='/' element={<Navigate to="/dashboard" replace />} />
-              <Route path="/dashboard" element={<Dashboard/>}/>
+              <Route path="/dashboard" element={<Dashboard />} />
             </Route>
           </Routes>
        </ThemeProvider>
