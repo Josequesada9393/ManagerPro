b24a26fc6c25479e34e0767207d5268213744f1f
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-05-26 16:38:41 -0700

    server side rendering & pagination

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,7 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
+import Transaction from "../models/Transactions.js";
 
 export const getProducts = async (req, res) => {
   try {
@@ -37,5 +38,43 @@ export const getCustomers = async (req, res) => {
 }
 
 export const getTransactions = async (req, res) => {
-  
+  try {
+    const {page = 1, pageSize = 20, sort = null, search = ""} = req.query;
+
+    const generateSort = () => {
+      const sortParsed = JSON.parse(soft);
+      const sortFormatted = {
+        [sortParsed.field]: sortParsed.sort = "asc" ? 1 : -1
+      };
+      return sortFormatted;
+    }
+
+    const sortFormatted = Boolean(sort) ? generateSort(): {};
+
+    const transactions = await Transaction.find({
+      //or allows to search multiple fields
+      $or: [
+        {cost: {$regex: new RegExp(search, "i")}},
+
+        {userId: {$regex: new RegExp(search, "i")}}
+      ]
+    })
+    .sort(sortFormatted)
+    .skip(page * pageSize)
+    .limit(pageSize)
+
+    const total = await Transaction.countDocuments({
+      name: {$regex: search, $options: "i"}
+    });
+
+    
+
+    res.status(200).json({
+      total,
+      transactions})
+    
+  } catch (error) {
+            res.status(404).json({message: error.message})
+
+  }
 }
