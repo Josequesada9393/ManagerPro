84118f01f02a207fb802457014b1a77fb9010840
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-05-30 16:01:07 -0700

    controller server sale transactions aggregate mongodb calls

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
 
 
 export const getAdmins = async (req, res) => {
@@ -10,3 +11,42 @@ export const getAdmins = async (req, res) => {
       res.status(404).json({ message: error.message });
     }
   };
+
+
+  export const getUserPerformance = async (req, res) => {
+    try {
+      const {id} = req.params;
+      const userWithStats = await User.aggretate([
+        // we find the user with that particular ID,
+        //we make sure that the ID matches the mongoDB ObjectId object
+        {$match: {_id: new mongoose.Types.ObjectId(id)}},
+        {
+          $lookup: {
+            //compare the User table with localfield _id with
+            //affiliateStats table with userID, return results as affiliateStats model
+            from: 'affiliatestats',
+            localField: "_id",
+            foreignField: "userId",
+            as: "affiliateStats"
+          }
+        },
+        //flaten the information
+        {$unwind: "$affiliateStats"}
+      ]);
+
+      const saleTransaction = await Promise.all(
+           userWithStats[0].affiliateStats.affiliateSales.map((id) => {
+          return Transaction.findById(id)
+        })
+      );
+
+      const filteredSaleTransaction = saleTransaction.filter((transaction) => transaction !== null);
+
+      res.status(200).json({user: userWithStats[0], sales: filteredSaleTransaction})
+  
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  };
+
+  
