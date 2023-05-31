7092d2167aef5ab9a5e91b834f3038960479b5e5
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-05-30 17:23:08 -0700

    controller dashboard with overall data

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,4 +1,7 @@
 import User from '../models/User.js'
+import OverallStat from '../models/OverallStat.js';
+import Transaction from '../models/Transaction.js';
+
 
 
 
@@ -10,5 +13,49 @@ export const getUser = async (req, res) => {
 
   } catch (error) {
     res.status(404).json({message: error.message})
+  }
+}
+
+export const getDashboardStats = async (req, res) => {
+  try {
+    //hardcoded values
+
+    const currentMonth = "November";
+    const currentYear = 2021;
+    const currentDay = "2021-11-15";
+
+    //Recent transactions
+
+    const transactions = await Transaction.find().limit(50).sort({createdOn: -1});
+
+    //overall stats
+
+    const overallStats = await OverallStat.find({year: currentYear});
+
+    const {totalCustomers, 
+    yearlySalesTotal,
+    yearlyTotalSoldUnits, 
+     monthlyData,
+     salesByCategory} = overallStats[0];
+
+     const thisMonthStats = overallStats[0].monthlyData.find(({month}) => {
+      return month === currentMonth;
+     })
+
+     const todayStats = overallStats[0].dailyData.find(({date}) => {
+      return date === currentDay;
+     });
+
+     res.status(200).json({totalCustomers, 
+      yearlySalesTotal,
+      yearlyTotalSoldUnits, 
+       monthlyData,
+       salesByCategory,
+      thisMonthStats,
+    todayStats,
+  transactions})
+
+  } catch (error) {
+    
   }
 }
